068f34a0fa8f2f86cad01210b48a80b9771f4641
maxikg <maxmax58@googlemail.com>
2015-12-27 20:00:14 +0100

    Added ability for specify queue options for hypixel and minecraft-uuid. Persistent messages by default.

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
         url: process.env['AMQP_URL'] || 'amqp://localhost/api'
     },
     apis: [
-        require('./lib/api/hypixel')({ api_key: process.env['HYPIXEL_KEY'], queue: process.env['HYPIXEL_QUEUE'] || 'requests_hypixel' }),
-        require('./lib/api/minecraft-uuid')({ queue: process.env['MOJANG_UUID_QUEUE'] || 'requests_mojang_uuid' })
+        require('./lib/api/hypixel')({ api_key: process.env['HYPIXEL_KEY'], queue: process.env['HYPIXEL_QUEUE'] || 'requests_hypixel', message_options: { persistent: true } }),
+        require('./lib/api/minecraft-uuid')({ queue: process.env['MOJANG_UUID_QUEUE'] || 'requests_mojang_uuid', message_options: { persistent: true } })
     ]
 };
