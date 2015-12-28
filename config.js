66222d0472fa1d80ed17ccd459abc416a0893a02
maxikg <maxmax58@googlemail.com>
2015-12-28 18:36:18 +0100

    Add logger configuration

--- config.js
+++ config.js
@@ -2,6 +2,11 @@ module.exports = {
     amqp: {
         url: process.env['AMQP_URL'] || 'amqp://localhost/api'
     },
+    log: {
+        appenders: [
+            { type: "console" }
+        ]
+    },
     apis: [
         require('./lib/api/hypixel')({ api_key: process.env['HYPIXEL_KEY'], queue: process.env['HYPIXEL_QUEUE'] || 'requests_hypixel', message_options: { persistent: true } }),
         require('./lib/api/minecraft-uuid')({ queue: process.env['MOJANG_UUID_QUEUE'] || 'requests_mojang_uuid', message_options: { persistent: true } })
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var config = require('./config');
-var logger = require('log4js').getLogger('main');
+var log4js = require('log4js');
+log4js.configure(config.log || {});
+var logger = log4js.getLogger('main');
 
 logger.info("Starting...");
 require('amqplib/callback_api').connect(config.amqp.url, function(err, conn) {
